a55dbcef4954c041524697812963841a2c5fddd3
momikimasashi <mngjxd03170317@gmail.com>
2018-01-16 19:57:14 +0900

    add NAND , OR code

--- chapter/chapter2/and.py
+++ chapter/chapter2/and.py
@@ -20,23 +20,32 @@ def ANDnp(x1, x2):
   else:
     return 1
 
+def NAND(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([-0.5, -0.5])
+  b = 0.7
+  tmp = np.sum(w * x) + b
+  if tmp <= 0:
+    return 0
+  else:
+    return 1
+
+def OR(x1, x2):
+  x = np.array([x1, x2])
+  w = np.array([0.5, 0.5])
+  b = -0.2
+  tmp = np.sum(w * x) + b
+  if tmp <= 0:
+    return 0
+  else:
+    return 1
+
 if __name__ == '__main__':
-  result = AND(0, 0)
-  print(result)
-  result = ANDnp(0, 0)
-  print(result)
-
-  result = AND(0, 1)
-  print(result)
-  result = ANDnp(0, 1)
-  print(result)
-
-  result = AND(1, 0)
-  print(result)
-  result = ANDnp(1, 0)
-  print(result)
-
-  result = AND(1, 1)
-  print(result)
-  result = ANDnp(1, 1)
-  print(result)
+  print(NAND(0, 0))
+  print(NAND(0, 1))
+  print(NAND(1, 0))
+  print(NAND(1, 1))
+  print(OR(0, 0))
+  print(OR(0, 1))
+  print(OR(1, 0))
+  print(OR(1, 1))
